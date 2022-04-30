37e65a8647e271342649c6ecf211c9da0596f1e5
ajmarrocco <ajmarrocco@gmail.com>
2022-04-29 22:29:18 -0400

    add random function

--- fun-game-app.cpp
+++ fun-game-app.cpp
@@ -1,14 +1,32 @@
 #include <string>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 // uses namespace std
 using namespace std;
 
 void play_game(){
-    cout << "Game is being played!" << endl;
+    // random number between 1 and 250
+    int random = rand() % 251;
+    cout << random << endl;
+    cout << "Guess a number: ";
+    while(true){
+        int guess;
+        cin >> guess;
+        if(guess == random){
+            cout << "You win!";
+        } else if (guess < random){
+            cout << "Too low";
+        } else {
+            cout << "Too high";
+        }
+    }
 }
 
 int main() {
+    //seeds random number and changes every time
+    srand(time(NULL));
     int choice;
 
     do{
@@ -19,13 +37,16 @@ int main() {
         switch (choice)
             {
                 case 0:
+                // Quit option
                     cout << "You chose to quit!\n";
                     break;
                 case 1:
+                // play game option
                     play_game();
                     break;
                 default:
                     break;
             }
+        // executes while choice is not zero
     } while(choice != 0);
 }
